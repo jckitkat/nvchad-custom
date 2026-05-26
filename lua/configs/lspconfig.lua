da0448abd2eb1fe3d6728dfc78d0259e5e80b2af
JC <jcmertz@outlook.com>
2026-05-26 15:22:43 -0400

    mostly finished neovim config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,12 +1,18 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = {"clangd", "jedi-language-server", "lua-language-server", "rust-analyzer", "jdtls", "gopls"}
+local servers = {"clangd", "jedi-language-server", "lua-language-server", "rust-analyzer", "jdtls", "gopls", "qmlls"}
 vim.lsp.enable(servers)
 
 vim.lsp.config("rust-analyzer", {
   cmd = { 'rust-analyzer' },
   root_markers = {'.git', 'Cargo.toml'},
   filetypes = {'rust'},
+})
+
+vim.lsp.config("jedi-language-server", {
+  cmd = { 'jedi-language-server' },
+  root_markers = {'.git', 'requirements.txt', 'venv'},
+  filetypes = {'python'},
 })
 
 -- read :h vim.lsp.config for changing options of lsp servers 
